c3885b259a3315eb8ed97144ddf38c3dab482d3d
Steffen R. Knollmann <satlank@gmx.de>
2010-07-13 23:24:45 +0200

    Beautifying the statistics script.

--- scripts/show-statistics.sh
+++ scripts/show-statistics.sh
@@ -18,14 +18,36 @@ function showForDir ()
 	else
 		echo "$1:"
 	fi
-	echo -n "  Files: $(grep -v total $$.1.dat | wc -l)"
-	echo " ($(grep -v total $$.2.dat | wc -l))"
-	echo -n "  Lines: $(grep total $$.1.dat | awk '{print $1}')"
-	echo " ($(grep total $$.2.dat | awk '{print $1}'))"
-	echo -n "  Words: $(grep total $$.1.dat | awk '{print $2}')"
-	echo " ($(grep total $$.2.dat | awk '{print $2}'))"
-	echo -n "  Chars: $(grep total $$.1.dat | awk '{print $3}')"
-	echo " ($(grep total $$.2.dat | awk '{print $3}'))"
+	numFilesTotal=$(grep -v total $$.1.dat | wc -l)
+	numFilesTest=$(grep -v total $$.2.dat | wc -l)
+	numFilesProduction=$(($numFilesTotal - $numFilesTest))
+	echo "  Files: $numFilesProduction $numFilesTest $numFilesTotal"
+	numLinesTotal=$(grep total $$.1.dat | awk '{print $1}')
+	numLinesTest=$(grep total $$.2.dat | awk '{print $1}')
+	if [[ ! $numLinesTest ]]
+	then
+		numLinesTest=0
+	fi
+	numLinesProduction=$(($numLinesTotal - $numLinesTest))
+	echo "  Lines: $numLinesProduction $numLinesTest $numLinesTotal"
+	numWordsTotal=$(grep total $$.1.dat | awk '{print $2}')
+	numWordsTest=$(grep total $$.2.dat | awk '{print $2}')
+	numLinesTest=$(grep total $$.2.dat | awk '{print $1}')
+	if [[ ! $numWordsTest ]]
+	then
+		numWordsTest=0
+	fi
+	numWordsProduction=$(($numWordsTotal - $numWordsTest))
+	echo "  Words: $numWordsProduction $numWordsTest $numWordsTotal"
+	numCharsTotal=$(grep total $$.1.dat | awk '{print $3}')
+	numCharsTest=$(grep total $$.2.dat | awk '{print $3}')
+	numLinesTest=$(grep total $$.2.dat | awk '{print $1}')
+	if [[ ! $numCharsTest ]]
+	then
+		numCharsTest=0
+	fi
+	numCharsProduction=$(($numCharsTotal - $numCharsTest))
+	echo "  Chars: $numCharsProduction $numCharsTest $numCharsTotal"
 	echo ""
 
 	rm -rf $$.1.dat $$.2.dat
@@ -38,5 +60,5 @@ showForDir src/libcosmo
 showForDir src/libutil
 showForDir src
 echo ""
-echo "Numbers in parenthesis are test code only."
+echo "Numbers are: [production code] [test code] [total]"
 echo ""
