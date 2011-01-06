fe7a6af46427763ee802f7f9b5cab816038ee232
Steffen R. Knollmann <satlank@gmx.de>
2011-01-06 16:30:35 +0000

    Working on the documentation.

---
+++ doc/doc.h
@@ -0,0 +1,51 @@
+// Copyright (C) 2011 Steffen Knollmann
+// Released under the terms of the GNU General Public License version 3.
+// This file is part of `ginnungagap'.
+
+/*
+ * IMPORTANT
+ * IMPORTANT  This files contains no useful code.  It only contains part
+ * IMPORTANT  of the package documentation in doxygen format.  The file
+ * IMPORTANT  only has the .h extension to ensure that doxygen uses the
+ * IMPORTANT  correct language parser for this file and to enable
+ * IMPORTANT  automatic syntax highlighting.
+ * IMPORTANT
+ */
+
+/**
+ * \mainpage Reference documentation for ginnungagap
+ *
+ * \author  Steffen Knollmann\n Grupo de Astrofísica\n Departamento de
+ * Física Teórica C-15\n Universidad Autónoma de Madrid\nCantoblanco
+ * 28049, Madrid\n steffen.knollmann@uam.es
+ *
+ * \n
+ *
+ * \section intro Introduction
+ *
+ * Ginnungagap is a code for generating cosmological initial conditions.
+ * It is aimed at high scalability and ease of use.
+ *
+ * \section requirements  Requirements
+ *
+ * To successfully compile ginnungagap, the following prerequisites must
+ * be met:
+ *
+ * - <b>C compiler</b> - Since ginnungagap uses features specific to
+ *   C99, a compiler supporting this standard is required.
+ * - <b>Build tools</b> - A make command should be available.
+ *   Ginnugagap has been tested with GNU make and other makes might not
+ *   support all features used in the Makefiles.  In case make is not
+ *   GNU make, your system has most likely gmake available.
+ * - <b>Shell</b> - A couple of Shell scripts need to be executed during
+ *   the build process, most prominently the configure script.  They are
+ *   supposed to work on POSIX shells and are tested on bash and dash.
+ *
+ * Additionally, the following libraries should be available to make
+ * full use of ginnungagap:
+ *
+ * - <b>SPRNG</b> -
+ * - <b>MPI</b> - 
+ * - <b>Silo</b> -
+ * - <b>HDF5</b> -
+ */
