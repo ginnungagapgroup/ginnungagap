e1b9241bfecc12b5dee1ff73294a54f9d0587d90
Steffen R. Knollmann <satlank@gmx.de>
2011-05-11 22:37:34 +0200

    [BROKEN] Starts to implement libdata.
    This moves a bunch of files from libgrid and libpart to a new library
    libdata which will provide basic data things (primitive [vector]
    variables and composed objects like particles).
    
    Currently, this horribly breaks the code.

---
+++ src/libdata/dataConfig.h
@@ -0,0 +1,33 @@
+// Copyright (C) 2011, Steffen Knollmann
+// Released under the terms of the GNU General Public License version 3.
+// This file is part of `ginnungagap'.
+
+#ifndef DATACONFIG_H
+#define DATACONFIG_H
+
+
+/*--- Doxygen file description ------------------------------------------*/
+
+/**
+ * @file libdata/dataConfig.h
+ * @ingroup libdata
+ * @brief This is main configuration header that all files in this
+ *        library include.
+ */
+
+
+/*--- Includes ----------------------------------------------------------*/
+#include "../../config.h"
+#include <stdint.h>
+
+
+/*--- Doxygen group definitions -----------------------------------------*/
+
+/**
+ * @defgroup libdata Library for data types.
+ * @brief This library provides functionality to handle a variety of
+ *        (physical) quantifies.
+ */
+
+
+#endif
