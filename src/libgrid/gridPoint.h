8205b8fd48fa0af74b4e70f3baa2e9a033c2502b
Steffen R. Knollmann <sknollmann@gmail.com>
2012-01-30 13:10:18 +0000

    Documenation updates.
    Now config.h and version.h are fully documented.  Actually, the
    documentation is in the .in files, as they are used to auto-generate the
    actual files.

--- src/libgrid/gridPoint.h
+++ src/libgrid/gridPoint.h
@@ -1,9 +1,18 @@
-// Copyright (C) 2010, Steffen Knollmann
+// Copyright (C) 2010, 2012, Steffen Knollmann
 // Released under the terms of the GNU General Public License version 3.
 // This file is part of `ginnungagap'.
 
 #ifndef GRIDPOINT_H
 #define GRIDPOINT_H
+
+
+/*--- Doxygen file description ------------------------------------------*/
+
+/**
+ * @file libgrid/gridPoint.h
+ * @ingroup libgridPoint
+ * @brief  Provides grid points.
+ */
 
 
 /*--- Includes ----------------------------------------------------------*/
@@ -12,10 +21,27 @@
 
 
 /*--- Typedefs ----------------------------------------------------------*/
+
+/** @brief  A #NDIM tuple of integers. */
 typedef int      gridPointInt_t[NDIM];
+
+/** @brief  A #NDIM tuple of 32bit unsigned integers. */
 typedef uint32_t gridPointUint32_t[NDIM];
+
+/** @brief  A #NDIM tuple of doubles. */
 typedef double   gridPointDbl_t[NDIM];
+
+/** @brief  A #NDIM tuple of #fpv_t. */
 typedef fpv_t    gridPointFpv_t[NDIM];
+
+
+/*--- Doxygen group definitions -----------------------------------------*/
+
+/**
+ * @defgroup libgridPoint Points
+ * @ingroup libgrid
+ * @brief Defines various points to be used in the grids.
+ */
 
 
 #endif
