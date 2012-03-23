96d1f589ed010d2de549a9d4f991dc931f2dde56
Steffen R. Knollmann <sknollmann@gmail.com>
2012-03-23 03:04:28 +0000

    Documentation update.

--- tools/makeMask/makeMaskSetup.h
+++ tools/makeMask/makeMaskSetup.h
@@ -6,6 +6,15 @@
 #define MAKEMASKSETUP_H
 
 
+/*--- Doxygen file description ------------------------------------------*/
+
+/**
+ * @file makeMask/makeMaskSetup.h
+ * @ingroup  toolsMakeMaskSetup
+ * @brief  Deals with parsing information from the ini file.
+ */
+
+
 /*--- Includes ----------------------------------------------------------*/
 #include "makeMaskConfig.h"
 #include "../../src/libutil/parse_ini.h"
@@ -13,18 +22,31 @@
 
 
 /*--- ADT handle --------------------------------------------------------*/
+
+/** @brief  Defines the handle for the setup structure. */
 typedef struct makeMaskSetup_struct *makeMaskSetup_t;
 
 
 /*--- Implemention of main structure ------------------------------------*/
+
+/**
+ * @brief  The actual setup structure.
+ */
 struct makeMaskSetup_struct {
+	/** @brief  The size of grid for which to generate the mask. */
 	uint32_t baseGridSize1D;
+	/** @brief  The number of refinement levels to generate. */
 	uint32_t numLevels;
+	/** @brief  The refinement factor between the levels. */
 	uint32_t refinementFactor;
+	/** @brief  The base level of the mask. */
 	uint32_t baseRefinementLevel;
+	/** @brief  Gives the section name from which to construct the writer. */
 	char     *outSecName;
+	/** @brief  The Lagrangian region object holding the hi-res cells. */
 	lare_t   lare;
 #ifdef WITH_MPI
+	/** @brief  Stores the MPI process grid to parallelize the mask. */
 	int      nProcs[NDIM];
 #endif
 };
