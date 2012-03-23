8aa16f5e38cf4bcc4baaa826eff0c29dae822b3b
Steffen R. Knollmann <sknollmann@gmail.com>
2012-03-23 02:08:02 +0000

    Adjusts the project field for files from voelva.
    A variety of files are used in voelva and in ginnungagap, this updates
    the project fields of those files to ginnungagap.

--- src/libutil/art_tests.h
+++ src/libutil/art_tests.h
@@ -1,6 +1,6 @@
 // Copyright (C) 2011, Steffen Knollmann
 // Released under the terms of the GNU General Public License version 3.
-// This file is part of `voelva'.
+// This file is part of `ginnungagap'.
 
 #ifndef ART_TESTS_H
 #define ART_TESTS_H
--- src/libutil/cubepmFactory.h
+++ src/libutil/cubepmFactory.h
@@ -1,6 +1,6 @@
 // Copyright (C) 2011, Steffen Knollmann
 // Released under the terms of the GNU General Public License version 3.
-// This file is part of `voelva'.
+// This file is part of `ginnungagap'.
 
 #ifndef CUBEPMFACTORY_H
 #define CUBEPMFACTORY_H
